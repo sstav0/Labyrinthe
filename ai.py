844151b642b325e88bdfdedd274827200697cb4f
Nicolas Jurquet <nicolor01@hotmail.com>
2023-03-27 12:11:21 +0200

    Next move test

---
+++ ai.py
@@ -0,0 +1,24 @@
+def move(state: dict) -> dict:
+    """
+    Defines the move to execute
+
+    Parameters
+    ----------
+    state : dict -- Dictionary from the server containing the state of the game
+
+    Returns
+    -------
+    dict
+        - tile containing its orientation
+        - gate in which we insert the tile
+        - new position of the tile
+    """
+    def move_calculator() -> list:
+        return [state["tile"], "A", 1]
+
+    next_move = {
+        "tile": move_calculator()[0],
+        "gate": move_calculator()[1],
+        "new_position": move_calculator()[2]
+    }
+    return next_move
