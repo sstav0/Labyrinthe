d969837e8e79d9d4a8d44104337129214f0576f1
sstav0 <xavierallaud@gmail.com>
2023-04-29 13:59:46 +0200

    update

--- ai.py
+++ ai.py
@@ -119,133 +119,126 @@ def randomMove(board, tile, playersPos, playerIndex):
     gate = gates[random.randint(0, len(gates)-1)]
     return {"gate": gate, "tile": tile, "new_position": move}
 
-    
-def minimax(board: list, tile: dict, playersPos: list[int], playerIndex:int, targetID: int, opponent:int, aiLevel:int=2, depth:int = 4, alpha= float("-inf"), beta=float("inf"), timeLimit: float=0.3)->tuple:
-    current_board = Board(board, tile, playersPos[playerIndex])
-    
-    best_score = float('-inf')
-    
-    if depth == 0 or foundTreasure(current_board.getBoard(), playersPos, playerIndex, targetID) or noMove(current_board.getBoard(), playersPos, playerIndex):
-        if foundTreasure(current_board.getBoard(), playersPos, playerIndex, targetID) and playerIndex != opponent:
-            return 1000, None
-        elif noMove(current_board.getBoard(), playersPos, playerIndex) and playerIndex == opponent:
-            return 20, None
-        elif depth == 0 and playerIndex != opponent:
-            itemIndex = current_board.findItem(targetID)
-            if itemIndex != None:
-                return 200 - 5*rules.distance(playersPos[playerIndex], itemIndex), None
-            else:
-                return 0, None
-        else:
-            return -5*noMove(current_board.getBoard(), playersPos, playerIndex, parameter="int"), None
-    
-    
-    for move in legalMoves(current_board):
-        current_board.update(move["gate"])
-        child_board, freeTile = current_board.getBoard(), current_board.getFreeTile()
-        playersPos[playerIndex]=move["new_position"]
-        
-        score, _ = minimax(child_board, freeTile, playersPos, playerIndex%-2+1, targetID, opponent, aiLevel+1, depth -1, alpha, beta)
-        
-        if score > best_score:
-            best_score, best_move = score, move
-        
-        if playerIndex != opponent:
-            alpha = max(alpha, score)
-            if beta <= alpha:
-                break
-        else:
-            beta = min(beta, score)
-            if beta <= alpha:
-                break
-        
-        current_board.undo()
-    return best_score, best_move
-    
-    
+#####################################################################################################
 
-# @timeit
-def negamaxPruning(board: list, tile: dict, playersPos: list[int], playerIndex: int, targetId: int, aiLevel: int, depth: int = 3, alpha=float("-inf"), beta=float("inf"), timeLimit: float = 3.0) -> tuple:
-    """
-    Negamax algorithm using alpha-beta pruning to find the best move (with the best value) for a given state of the game.
+def findItem(board:list, item: int):
+    """This function returns the position of [item]
 
     Parameters
     ----------
-    board : list
-        Current state of the board.
-    tile : dict
-        Current free tile to be inserted.
-    playersPos : list[int]
-        List containing the index of the tile at the position of each player.
-    playerIndex : int
-        Index of the current player needed to pick the right index from `playersPos`.
-    targetId : int
-        Contains the ID of the treasure to reach (to not be confused with the index of the tile).
-    playersScore : list[int]
-        List containing the remaining treasures to reach by the two players.
-    aiLevel : int
-        Level of the AI from 1 to 3.
-    depth : int, optional
-        Depth of the recursive algorithm, by default 4.
-    alpha : int | float, optional
-        Actual highest move value, by default float("-inf").
-    beta : int | float, optional
-        Actual lowest move value, by default float("inf").
-    timeLimit : float, optional
-        Time limit in seconds of the recursive algorithm, by default 3.0
+    item : int
+        item number
 
     Returns
     -------
-    tuple[int, dict]
-        Best value with the best move to play.
+    int
+        position of the item 
     """
-    current_board = Board(board, tile, playersPos[playerIndex])
-    #print(board[playersPos[playerIndex]], "------------")
+    found = False
+    for i, tile in enumerate(board):
+        if tile["item"] == item:
+            found = True
+            return i
+    if not found:
+        print("ITEM {} NOT FOUND".format(chr(ord("A")+item)))
+
+def h(initPos, target):
+    if rules.distance(initPos, target)==0:
+        return 1000
+    elif rules.distance(initPos, target) <= 2:
+        return 500-10*rules.distance(initPos, target)
+    else: 
+        return 300-10*rules.distance(initPos, target)
     
-    # Returns the value of the board state when we are at the max depth and when we find a treasure
-    if depth == 0 or foundTreasure(current_board.getBoard(), playersPos, playerIndex, targetId):
-        if foundTreasure(current_board.getBoard(), playersPos, playerIndex, targetId):
-            print(f"\nPLAYERSPOS: {playersPos} ------ PLAYERINDEX:{playerIndex} -------- TARGETID: {targetId} ------- \n")
-            value = 834
-            return -value, None
-        return -1000000, None
 
-    start_time = time.time()
-    bestVal = -100000
-    bestMove = None
+def boardMap(board):
+    map_ = {}
+    for i, tile in enumerate(board): 
+        map_[i] = -100000
+    return map_
+
+def bestNode(openSet, target):
+    bestScore = float("-inf")
+    bestElem = None
+    for elem in openSet:
+        fScore = h(elem, target)
+        if fScore >= bestScore:
+            bestScore = fScore
+            bestElem = elem 
+    return bestElem
+
+def reconstructPath(cameFrom, current):
+    totalPath = [current]
+    while current in cameFrom:
+        current = cameFrom[current]
+        totalPath.insert(0, current)
+    return totalPath
+
+def playerLegalMoves(initialPos: int, board: list) -> list[int]:
+    """This function returns a list of the possible moves (positions)
+
+    Parameters
+    ----------
+    initialPos : int
+        initial position
+    board : list
+        list of the dictionaries referring to the walls and free ways of each tile on the board
+
+    Returns
+    -------
+    list 
+        list of the legal moves. It contains at least the initial position
+    """
+    board_legalMoves = []
+
+    # iters through the dictionary of the initial tile
+    for direction, value in board[initialPos].items():
+        if direction != "item":  # checks if it's not iterating through the item of the tile
+            if value == True:  # if the value is true means that there is no wall in that orientation
+                # nextTile is your next position if you'd move in the direction
+                nextTile = rules.nextIndex(initialPos, direction)
+                # checks if there is a wall in the direction on the next Tile (/!\ the next tile's direction is the opposite of the previous tile's direction)
+                if board[nextTile][rules.oppositeDirection(direction)] == True and nextTile != initialPos:
+                    # if there is no wall, it adds the next tile's index in the legal moves list
+                    board_legalMoves.append(nextTile)
+        else:
+            pass
+    return board_legalMoves
+
+def A_star(initPos, target, board):
+    targetPos = findItem(board, target)
+    openSet = [initPos]
     
-
-    for move in legalMoves(current_board):
-        current_board.update(move["gate"])
-        child_board, freeTile = current_board.getBoard(), current_board.getFreeTile()
-        playersPos[playerIndex]=move["new_position"]
-        # Checks further until depth = 0
-        value, _ = negamaxPruning(child_board, freeTile, playersPos,
-                                  playerIndex % -2 + 1, targetId, aiLevel, depth-1, -beta, -alpha)
-        # Keep the value and move with the best score for the current player
-        if value > bestVal:
-            #print(f"\nVALUE: {value} ----- MOVE: {move}")
-            bestVal = value
-            bestMove = move
-            print(bestVal, bestMove)
-        # Determines whether we keep searching or we break the branch
-        alpha = max(alpha, value)
-        if alpha >= beta:
-            break
-
-        # Check if the time limit has been reached
-        elapsed_time = time.time() - start_time
-        if elapsed_time >= timeLimit:
-            break
-        current_board.undo()
-    # Returns the negative so that the next player is choosing the opposite (min/max)
-    return -bestVal, bestMove
-
-# TODO Function that returns the value of a given move depending on the current state of the game
-def moveValue(level: int, playerIndex: int, parameter=False) -> int:
-    #playersScore = foundTreasure(board, playersPos, playerIndex, targetId, playersScore)[1]
-    if parameter:
-        score = 1000
-    else:
-        score = 0
-    return score
+    cameFrom = {}
+    
+    gScore = boardMap(board)
+    gScore[initPos]= 0
+    
+    fScore = boardMap(board)
+    fScore[initPos]= h(initPos, targetPos)
+    
+    while openSet != []:
+        
+        current = bestNode(openSet, targetPos)
+        print(current)
+        time.sleep(1)
+        if current == targetPos:
+            return reconstructPath(cameFrom, current)
+        if playerLegalMoves(current, board) != []:
+            
+            for neighbor in (playerLegalMoves(current, board)):
+                tentativeScore = gScore[current]+rules.distance(initPos, neighbor)
+                if tentativeScore < gScore[neighbor]:
+                    cameFrom[neighbor]=current
+                    gScore[neighbor]=tentativeScore
+                    fScore[neighbor]=tentativeScore+ h(neighbor, targetPos)
+                    if neighbor not in openSet:
+                        openSet.append(neighbor)
+                print(f"NEIGHBOR: {neighbor}; TENTATIVE: {tentativeScore};GSCORE: {gScore[neighbor]}; OPENSET:{openSet}")
+                time.sleep(2)
+                
+        else: 
+            return "CAN'T MOVE"
+    
+    return 'PATH NOT FOUND'
+            
