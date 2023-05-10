2a5504bc41f8a0568e4b48356da47b093d8d014c
Xavier Allaud <xavierallaud@gmail.com>
2023-05-10 11:46:15 +0200

    make tests

---
+++ tests/test_ai.py
@@ -0,0 +1,63 @@
+import Program.ai as ai 
+import Program.rules as rules
+import pytest 
+import copy
+
+@pytest.fixture
+def state_test():
+    state = {'players': ['AImazing1', 'AImazing0'], 'current': 0, 'positions': [0, 47], 'target': 17, 'remaining': [3, 4], 'tile': {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, 'board': [{'N': False, 'E': True, 'S': True, 'W': False, 'item': None}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': 14}, {'N': False, 'E': True, 'S': True, 'W': True, 'item': 0}, {'N': True, 'E': True, 'S': False, 'W': False, 'item': None}, {'N': False, 'E': True, 'S': True, 'W': True, 'item': 1}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': False, 'E': False, 'S': True, 'W': True, 'item': None}, {'N': True, 'E': False, 'S': True, 'W': False, 'item': None}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': 15}, {'N': False, 'E': False, 'S': True, 'W': True, 'item': None}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': None}, {'N': False, 'E': True, 'S': True, 'W': False, 'item': None}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': True, 'E': True, 'S': True, 'W': False, 'item': 2}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': 12}, {'N': True, 'E': True, 'S': True, 'W': False, 'item': 3}, {'N': True, 'E': False, 'S': True, 'W': False, 'item': None}, {'N': False, 'E': True, 'S': True, 'W': True, 'item': 4}, {'N': False, 'E': True, 'S': True, 'W': True, 'item': 21}, {'N': True, 'E': False, 'S': True, 'W': True, 'item': 5}, {'N': False, 'E': True, 'S': True, 'W': True, 'item': 19}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': 13}, {'N': True, 'E': True, 'S': False, 'W': True, 'item': 22}, {'N': True, 'E': False, 'S': True, 'W': True, 'item': 20}, {
+        'N': True, 'E': False, 'S': False, 'W': True, 'item': None}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': True, 'E': True, 'S': True, 'W': False, 'item': 6}, {'N': False, 'E': False, 'S': True, 'W': True, 'item': None}, {'N': True, 'E': True, 'S': False, 'W': True, 'item': 7}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': True, 'E': False, 'S': True, 'W': True, 'item': 8}, {'N': True, 'E': False, 'S': True, 'W': False, 'item': None}, {'N': True, 'E': False, 'S': True, 'W': True, 'item': 9}, {'N': False, 'E': True, 'S': True, 'W': False, 'item': None}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': 17}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': None}, {'N': True, 'E': False, 'S': True, 'W': True, 'item': 23}, {'N': True, 'E': False, 'S': True, 'W': True, 'item': 18}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': True, 'E': True, 'S': False, 'W': False, 'item': None}, {'N': True, 'E': True, 'S': False, 'W': False, 'item': None}, {'N': False, 'E': True, 'S': True, 'W': False, 'item': None}, {'N': True, 'E': True, 'S': False, 'W': True, 'item': 10}, {'N': True, 'E': True, 'S': False, 'W': False, 'item': 16}, {'N': True, 'E': True, 'S': False, 'W': True, 'item': 11}, {'N': False, 'E': True, 'S': False, 'W': True, 'item': None}, {'N': True, 'E': False, 'S': False, 'W': True, 'item': None}]}
+    return state
+
+@pytest.fixture
+def current_test(state_test):
+    return state_test["current"]
+
+@pytest.fixture
+def initPos_test(state_test, current_test):
+    return state_test["positions"][current_test]
+
+@pytest.fixture
+def target_test(state_test):
+    return state_test["target"]
+
+@pytest.fixture
+def board_test(state_test):
+    return state_test["board"]
+
+@pytest.fixture
+def pos_test(state_test):
+    return state_test["positions"]
+
+@pytest.fixture
+def blockedBoard_test(board_test):
+    bB = copy.deepcopy(board_test)
+    bB[0]["E"] = False
+    bB[0]["S"] = False
+    return bB
+
+@pytest.fixture
+def openSet_test(initPos_test, board_test):
+    return rules.playerLegalMoves(initPos_test, board_test)
+
+@pytest.fixture
+def tile_test(state_test):
+    return state_test["tile"]
+
+def test_h(initPos_test, target_test):
+    assert isinstance(ai.h(initPos_test, target_test), int)
+    
+def test_boardMap(board_test): 
+    assert isinstance(ai.boardMap(board_test), dict)
+
+def test_bestNode(openSet_test, target_test):
+    assert isinstance(ai.bestNode(openSet_test, target_test), int)
+
+def test_A_star(initPos_test, target_test, board_test, blockedBoard_test):
+    assert isinstance(ai.A_star(initPos_test, target_test, board_test), list)
+    assert isinstance(ai.A_star(initPos_test, target_test, blockedBoard_test), str)
+
+def test_makeMove(tile_test, pos_test, current_test, target_test, board_test):
+    assert isinstance(ai.makeMove(tile_test, pos_test, current_test, target_test, board_test), dict)
+
+    
